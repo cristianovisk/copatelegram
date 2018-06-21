c400ed3cf6c5d538fb3e45036f297778d1a4acb4
cristianovisk <cristianovisk@gmail.com>
2018-06-21 11:46:33 -0300

    Update copa.py

--- copa.py
+++ copa.py
@@ -6,14 +6,21 @@ import telepot
 from googletrans import Translator()
 
 telegram=telepot.Bot('TOKEN_BOT_TELEGRAM')
-destiny='CHATID_RECEIVER'
+destiny=['CHATID_RECEIVER'] #Type list
 ultim_jogo=''
 flag=''
+print('')
 tr=Translator()
+
+#CHECK VARIABLES ------------------------------------
 if destiny == 'CHATID_RECEIVER':
     print ("Change variable 'destiny' with CHATID and 'telegram' with TOKEN_BOT in code.")
     exit()    
 
+#SEND MSG TO TELEGRAM ------------------------------------
+def sendMSG(MSG):
+    for user in destiny:
+            telegram.sendMessage(user, MSG, disable_web_page_preview=True)
 
 #CHECK WEBSERVER COPA ------------------------------------
 def check():
@@ -32,16 +39,18 @@ def flag_check(ultim_jogo):
     flag=ultim_jogo
     return flag;
 
+sendMSG("BOT Reiniciado...")
+
 while True:
     flag=flag_check(ultim_jogo)
     ultim_jogo=(str(check()).decode('utf-8').replace(',','').replace('(','').replace(')','').replace("'","").replace('x u', 'x '))
     ultim_jogo=("xs %s" %ultim_jogo).replace('xs u', '')
 
     if flag != ultim_jogo:
-    	print ("Goool!!!")
-    	print (ultim_jogo)
-        telegram.sendMessage(destiny, tr.translate(ultim_jogo, src='en', dest='pt').text, disable_web_page_preview=True)
+        print ("Goool!!!")
+        print (ultim_jogo)
+        sendMSG(tr.translate(ultim_jogo, src='en', dest='pt').text)
     else:
-    	print ("Nothing... Sleep 15...")
-    	print (ultim_jogo)
-    	time.sleep(15)
+        print ("Nothing... Sleep 15...")
+        print (ultim_jogo)
+        time.sleep(15)
